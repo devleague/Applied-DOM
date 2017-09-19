5f3bd519db36298cf2f7dd0620b0a9c7f09523bf
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-18 18:31:25 -1000

    Eight button working

--- js/app.js
+++ js/app.js
@@ -158,7 +158,9 @@ btn7.addEventListener("click", () => {
    *   and passing two aditional arguments, event and 'light-green'
    */
 
-
+btn8.addEventListener("click", function(event){
+  setMyLightClass.call(btn8, event, "light-green");
+});
   /*
    * Add a click event listener to btn9
    * the handler method will be a fat arrow function expression
