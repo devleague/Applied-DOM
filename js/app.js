b1572f0f6bb43bda3b18b755a3a150266f16fff7
cryosx <cyruswu.email@gmail.com>
2018-04-10 15:41:47 -1000

    btn11 done

--- js/app.js
+++ js/app.js
@@ -205,12 +205,18 @@
    *     null, 'light-green'
    */
 
+  const setLight11Green = setMyLightClass.bind(btn11, null, 'light-green');
+
   /*
    * Add a click event listener to btn11
    * the handler method will be an anonymous function expression
    * the body of the event handler function
    * will invoke the setLight11Green function
    */
+
+  btn11.addEventListener('click', function() {
+    setLight11Green();
+  });
 
   /*
    * Declare a new const named setLight12Class
