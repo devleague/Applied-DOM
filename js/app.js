db35e74a9d01f1320578b232c03bbc133dc588e9
Bronson Avila <bronson.avila@gmail.com>
2018-07-10 15:59:36 -1000

    Complete btn7

--- js/app.js
+++ js/app.js
@@ -144,7 +144,9 @@ btn6.addEventListener('click', function() {
    * using the Function prototype method: call
    * to set the context to the correct object
    */
-
+  btn7.addEventListener('click', () => {
+    setMyLightGreen.call(event.currentTarget);
+  })
 
   /*
    * Add a click event listener to btn8
