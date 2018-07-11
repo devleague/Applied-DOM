eb56928ff246226707680efed63b0685670360f5
Bronson Avila <bronson.avila@gmail.com>
2018-07-10 16:27:27 -1000

    Change btn9 event listener to refer directly to btn9

--- js/app.js
+++ js/app.js
@@ -172,7 +172,7 @@
    *   and passing two aditional arguments, event and 'light-green'
    */
   btn9.addEventListener('click', event => {
-    setMyLightClass.call(event.target, event, 'light-green');
+    setMyLightClass.call(btn9, event, 'light-green');
   });
 
   /*
